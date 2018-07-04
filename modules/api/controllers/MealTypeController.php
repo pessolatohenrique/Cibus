78cd71ec8815206ac8ad35c8ca0adc68b60e8f5b
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-07-04 09:16:05 -0300

    API - Listagem e pesquisa de tipos de refeicao

---
+++ modules/api/controllers/MealTypeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\filters\auth\HttpBearerAuth;
+
+use app\models\Refeicao;
+use app\models\RefeicaoSearch;
+
+class MealTypeController extends ActiveController
+{
+    public $modelClass = 'app\models\Refeicao';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::className(),
+        ];
+        return $behaviors;
+    }
+    
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
+
+    public function actionIndex() {
+        $searchModel = new RefeicaoSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $dataProvider;
+    }
+}
